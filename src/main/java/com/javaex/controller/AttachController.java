9b437734b6ffeb8aca8beccec6c1a8447ab68e9e
황덕룡 <hwangsee12@naver.com>
2024-09-03 16:16:24 +0900

    2024-09-03

--- src/main/java/com/javaex/controller/AttachController.java
+++ src/main/java/com/javaex/controller/AttachController.java
@@ -2,9 +2,11 @@ package com.javaex.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.javaex.service.AttachService;
@@ -20,17 +22,20 @@ public class AttachController {
    public String form() {
       System.out.println("AttachController.form()");
       
-      return "attach/form";
+      return "/attach/form";
    }
    
    /* 업로드 */
+   
    @RequestMapping(value = "/attach/upload", method = { RequestMethod.GET, RequestMethod.POST })
-   public String upload(@RequestParam(value="file") MultipartFile file) {
+   public String upload(@RequestParam(value="file") MultipartFile file, Model model) {
       System.out.println("AttachController.upload()");
       System.out.println(file.getOriginalFilename());
-      attachService.upload(file);
+      String saveName=attachService.upload(file);
+      System.out.println(saveName);
       
-      return "";
+      model.addAttribute("saveName",saveName);
+      return "/attach/result";
    }
 
 }
--- src/main/java/com/javaex/service/AttachService.java
+++ src/main/java/com/javaex/service/AttachService.java
@@ -1,30 +1,73 @@
 package com.javaex.service;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.javaex.vo.AttachVo;
 
 @Service
 public class AttachService {
 
-	public void upload(MultipartFile file) {
+	public String upload(MultipartFile file) {
 		System.out.println("AttachService.upload");
 		System.out.println(file.getOriginalFilename());
-	
-		//사진에 기본정보로 우리가 관리할 정보를 뽑아내야한다
-		//사진을 서버에 복사해야한다
-		//오리지날 파일명
-		String orgName =file.getOriginalFilename();
-		System.out.println("orgName: "+ orgName);
-		
-		//확장자
-		String exeName=orgName.substring(orgName.lastIndexOf("."));
-		
-		System.out.println(exeName); 
-		
-		//파일 사이즈
-		long FileSize=file.getSize();
-		System.out.println(FileSize);
-		
-		//사진을 서버(강남)에 하드디스크에 복사해야한다
+
+		// 사진에 기본정보로 우리가 관리할 정보를 뽑아내야한다-->db저장
+		// 파일 저장 폴더
+
+		String saveDir = "C:\\javaStudy\\upload";
+
+		// 사진을 서버에 복사해야한다
+		// 오리지날 파일명
+		String orgName = file.getOriginalFilename();
+		System.out.println("orgName: " + orgName);
+
+		// 확장자
+		String exeName = orgName.substring(orgName.lastIndexOf("."));
+
+		System.out.println(exeName);
+
+		// 파일 사이즈
+		long fileSize = file.getSize();
+		System.out.println(fileSize);
+
+		// 저장파일명(겹치지않아야한다)
+		String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exeName;
+		System.out.println("saveName: " + saveName);
+
+		// 파일전체경로
+		String filePath = saveDir + "\\" + saveName;
+		System.out.println("filePath: " + filePath);
+
+		// (1)DB저장
+		// (1-1) 데이터 묶기
+		AttachVo attachVo = new AttachVo(orgName, saveName, filePath, fileSize);
+		System.out.println(attachVo);
+		System.out.println("과제:DB저장 중");
+
+		// (1-2) dao 통해서 db에 저장
+		// 과제
+
+		// (2)사진을 서버(강남)에 하드디스크에 복사해야한다
+		// 파일 저장
+
+		try {
+			byte[] fileData = file.getBytes();
+
+			OutputStream os = new FileOutputStream(filePath);
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+
+			bos.write(fileData);
+			bos.close();
+
+		} catch (Exception e) {
+			System.out.println();
+		}
+			return saveName;
 	}
 }
