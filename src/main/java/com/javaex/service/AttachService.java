46f9ca6c7daf832be44a0f299408201334a4278a
황덕룡 <hwangsee12@naver.com>
2024-09-03 18:31:23 +0900

    2024-09-03

--- src/main/java/com/javaex/service/AttachService.java
+++ src/main/java/com/javaex/service/AttachService.java
@@ -5,24 +5,26 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.javaex.dao.AttachDao;
 import com.javaex.vo.AttachVo;
 
 @Service
 public class AttachService {
 
+	@Autowired
+	private AttachDao attachDao;
+
 	public String upload(MultipartFile file) {
 		System.out.println("AttachService.upload");
-		System.out.println(file.getOriginalFilename());
-
 		// 사진에 기본정보로 우리가 관리할 정보를 뽑아내야한다-->db저장
-		// 파일 저장 폴더
 
+		// 파일 저장 폴더
 		String saveDir = "C:\\javaStudy\\upload";
 
-		// 사진을 서버에 복사해야한다
 		// 오리지날 파일명
 		String orgName = file.getOriginalFilename();
 		System.out.println("orgName: " + orgName);
@@ -30,19 +32,14 @@ public class AttachService {
 		// 확장자
 		String exeName = orgName.substring(orgName.lastIndexOf("."));
 
-		System.out.println(exeName);
-
 		// 파일 사이즈
 		long fileSize = file.getSize();
-		System.out.println(fileSize);
 
 		// 저장파일명(겹치지않아야한다)
 		String saveName = System.currentTimeMillis() + UUID.randomUUID().toString() + exeName;
-		System.out.println("saveName: " + saveName);
 
 		// 파일전체경로
 		String filePath = saveDir + "\\" + saveName;
-		System.out.println("filePath: " + filePath);
 
 		// (1)DB저장
 		// (1-1) 데이터 묶기
@@ -51,23 +48,25 @@ public class AttachService {
 		System.out.println("과제:DB저장 중");
 
 		// (1-2) dao 통해서 db에 저장
-		// 과제
+		int count = attachDao.Attachinsert(attachVo);
 
 		// (2)사진을 서버(강남)에 하드디스크에 복사해야한다
 		// 파일 저장
+		if (count == 1) {
+			try {
+				byte[] fileData = file.getBytes();
 
-		try {
-			byte[] fileData = file.getBytes();
+				OutputStream os = new FileOutputStream(filePath);
+				BufferedOutputStream bos = new BufferedOutputStream(os);
 
-			OutputStream os = new FileOutputStream(filePath);
-			BufferedOutputStream bos = new BufferedOutputStream(os);
+				bos.write(fileData);
+				bos.close();
 
-			bos.write(fileData);
-			bos.close();
-
-		} catch (Exception e) {
-			System.out.println();
-		}
+			} catch (Exception e) {
+				System.out.println();
+			}
 			return saveName;
+		} else
+			return null;
 	}
 }
